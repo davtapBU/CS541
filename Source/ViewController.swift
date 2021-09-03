9f90bcbed30ecd2f4152d44efdacad6ceefc36c3
David Tapia <flawie@Davids-MacBook-Pro.local>
2021-09-03 10:38:36 -0400

    Copied source to separate folder for ease of access

---
+++ Source/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by David Tapia on 9/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.becomeFirstResponder()
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        get {
+            return true
+        }
+    }
+
+    let url = URL(string: "http://www.weather.com")!
+    
+    // Enable detection of shake motion
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            //let alertController = UIAlertController(title: "Ah!", message: "Don't shake me!", preferredStyle: UIAlertController.Style.alert)
+           // alertController.addAction(UIAlertAction(title: "Sorry", style: UIAlertAction.Style.default, handler: nil))
+           // present(alertController, animated: true, completion: nil)
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+    
+    @IBOutlet var mySwitch : UISwitch!
+    @IBOutlet var myButton : UIButton!
+
+    @IBAction func showMessage(sender: UIButton) {
+        if mySwitch.isOn {
+            let alertController = UIAlertController(title: "Alert", message: "Hello World!", preferredStyle: UIAlertController.Style.alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+        
+        else {
+            let alertController = UIAlertController(title: "Alert", message: "Goodnight World!", preferredStyle: UIAlertController.Style.alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func changeTheme(sender: UISwitch) {
+        if mySwitch.isOn {
+            self.view.backgroundColor = UIColor.systemTeal
+            myButton.setTitle("☀️", for: .normal)
+        }
+        else {
+            self.view.backgroundColor = UIColor.systemIndigo
+            myButton.setTitle("🌙", for: .normal)
+        }
+    }
+}
+
